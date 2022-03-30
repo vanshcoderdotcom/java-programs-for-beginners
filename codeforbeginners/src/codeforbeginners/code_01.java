e4668e15e3b0cc6e712c90168f5ca805ebaed18b
Vansh <Vansh@DESKTOP-GODON9B>
2022-03-30 21:12:47 +0530

    first commit..

---
+++ codeforbeginners/src/codeforbeginners/code_01.java
@@ -0,0 +1,13 @@
+package codeforbeginners;
+
+/*  first java program*/
+
+public class code_01 {
+
+	public static void main(String[] args) {
+		
+System.out.println(" first java program"); // print the statement
+System.out.println(" hello world \n"+" welcome to my tutorial friends "); //print more than one statement  
+	}
+
+}
